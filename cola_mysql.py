fd971f2e2e84c2b000a2cc4e4a8024dcff9c3237
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-06-20 11:57:32 -0600

    mejora cola

--- cola_mysql.py
+++ cola_mysql.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import time
+import requests
 
 # Conexión a MySQL
 cnx = mysql.connector.connect(user='root', password='password', host='127.0.0.1', database='my_database')
@@ -12,26 +13,31 @@ values = (data, 'unread', None, time.time())
 cursor.execute(query, values)
 cnx.commit()
 
+
 # Procesar cada elemento no leído de la cola
 while True:
-    # Leer hasta 10 elementos de la cola
-    query = "SELECT id, data FROM queue_table WHERE status = 'unread' LIMIT 10 FOR UPDATE"
+    # Leer un solo elemento de la cola
+    query = "SELECT id, data FROM queue_table WHERE status = 'unread' LIMIT 1 FOR UPDATE"
     cursor.execute(query)
-    items = cursor.fetchall()
-    if items:
-        for item in items:
-            id, data = item
-
+    item = cursor.fetchone()
+    if item:
+        id, data = item
+        try:
             # Aquí puedes procesar los datos del elemento de la forma que necesites
+            api_url = "https://jsonplaceholder.typicode.com/todos/1"
+            response = requests.get(api_url)
+            if response.status_code == 200:
+                # Registrar qué servidor procesó el elemento
+                query = "UPDATE queue_table SET status = 'read', processed_by = %s WHERE id = %s"
+                values = ("Server_1", id)  # Aquí debes obtener el nombre o ID de tu servidor de alguna manera
+                cursor.execute(query, values)
+                cnx.commit()
+            else:
+                raise Exception("API call failed")
+        except Exception as e:
+            print(f"Error processing item {id}: {e}")
+            cnx.rollback()
 
-            # Registrar qué servidor procesó el elemento
-            query = "UPDATE queue_table SET status = 'read', processed_by = %s WHERE id = %s"
-            values = ("Server_1", id)  # Aquí debes obtener el nombre o ID de tu servidor de alguna manera
-            cursor.execute(query, values)
-
-        cnx.commit()
-    
-        
     # Comprobar si quedan elementos no leídos en la cola
     query = "SELECT COUNT(*) FROM queue_table WHERE status = 'unread'"
     cursor.execute(query)
@@ -41,4 +47,5 @@ while True:
         time.sleep(60)
 
 cursor.close()
+
 cnx.close()
